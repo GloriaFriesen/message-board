f99a05a8f5aed4398caa52e34fd5374ce1aba839
Gloria Friesen <gloriafriesen@gmail.com>
2017-04-28 20:05:36 -0700

    add new-answer component, add one-to-many relationship between question and answers

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   query: DS.attr(),
   author: DS.attr(),
-  note: DS.attr()
+  note: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
